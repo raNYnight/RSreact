6e3148fd2b6daceebd02c5d572299c49c6c92123
Ilya <rany4ka@gmail.com>
2023-12-02 14:31:15 +0300

    fix: pass str indicator

--- src/components/second-form/password-field/password-field.tsx
+++ src/components/second-form/password-field/password-field.tsx
@@ -28,12 +28,32 @@ export const PasswordField = (props: PasswordFieldProps) => {
   );
 
   const calculatePasswordStrength = (password: string) => {
-    if (password.length < 6) {
-      return 'Weak';
-    } else if (password.length < 10) {
-      return 'Medium';
-    } else {
-      return 'Strong';
+    let strength = 0;
+    if (password.length >= 6) {
+      strength++;
+    }
+    if (/[A-Z]/.test(password)) {
+      strength++;
+    }
+    if (/[!@#$%^&*]/.test(password)) {
+      strength++;
+    }
+    if (/\d/.test(password)) {
+      strength++;
+    }
+    switch (strength) {
+      case 0:
+        return 'Very Weak';
+      case 1:
+        return 'Weak';
+      case 2:
+        return 'Medium';
+      case 3:
+        return 'Strong';
+      case 4:
+        return 'Very Strong';
+      default:
+        return '';
     }
   };
 
